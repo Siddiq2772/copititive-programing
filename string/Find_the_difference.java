16419fb141e26b6be949fa1da6aa0ff02298c09e
mohd. siddique <siddiqmohd2772@gmail.com>
2024-09-07 22:50:50 +0530

    in java

--- string/Find_the_difference.java
+++ string/Find_the_difference.java
@@ -5,7 +5,7 @@ public class Find_the_difference {
 
         String s1 ="my name is siddiq",//old array
                s2 = "my name is mohd siddique";//new array
-
+            //logic 1
         char[] a1 =s1.toCharArray(),
                 a2 = s2.toCharArray();
             System.out.println("modified characters: ");
@@ -15,9 +15,22 @@ public class Find_the_difference {
                     System.out.println(a2[i]+"  ");
             }else
                 System.out.println(a2[i]+"  ");
+                      
                 
-                
-                
+        }
+         //logic 2
+         String[] sa1 = s1.split(" "),
+         sa2 = s2.split(" ");
+        System.out.println("modification made:");
+        for (int i = 0; i < sa2.length; i++) {
+            if(sa1.length>i){
+                if(!sa2[i].equals(sa1[i]))
+                    System.out.print(sa2[i]+", ");            
+                 
+            }                
+            else
+                System.out.print(sa2[i]+", ");           
+
         }
     }
     
